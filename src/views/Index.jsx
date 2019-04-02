f6c4432e5fef186558e648293f5e6bcf4dc0ee50
Daniel Pittman <dpdanpittman@gmail.com>
2019-04-02 00:13:02 -0700

    Update Index.jsx

--- src/views/Index.jsx
+++ src/views/Index.jsx
@@ -21,6 +21,7 @@ class Index extends React.Component {
       <>
         <IndexNavbar />
         <div className="wrapper">
+		
           <PageHeader />
           <div className="main">
             <Basics />
