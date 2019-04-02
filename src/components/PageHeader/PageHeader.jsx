62fe505a7bb896d4ee4a2a55c9018d8692cf131c
Daniel Pittman <dpdanpittman@gmail.com>
2019-04-02 01:21:31 -0700

    Update PageHeader.jsx

--- src/components/PageHeader/PageHeader.jsx
+++ src/components/PageHeader/PageHeader.jsx
@@ -7,6 +7,7 @@ class PageHeader extends React.Component {
     return (
       <div className="page-header header-filter">
         <div className="squares square1" />
+		<div className="squares square4" />
         <Container>
           <div className="content-center brand">
             <h1>
