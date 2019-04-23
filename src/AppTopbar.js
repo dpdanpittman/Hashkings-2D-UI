a785461a01712df8d0a287031e2d1332e8476655
Daniel Pittman <dpdanpittman@gmail.com>
2019-04-23 16:13:17 -0700

    Update AppTopbar.js

--- src/AppTopbar.js
+++ src/AppTopbar.js
@@ -15,22 +15,6 @@ export class AppTopbar extends Component {
         return (
             <div className="layout-topbar clearfix">
                 <div className="layout-topbar-icons">
-                    <button className="p-link">
-                        <span className="layout-topbar-item-text">User</span>
-                        <span className="layout-topbar-icon pi pi-user"/>
-                    </button>
-					<button className="p-link">
-                        <span className="layout-topbar-item-text">Events</span>
-                        <span className="layout-topbar-icon pi pi-calendar"/>
-                    </button>
-                    <button className="p-link">
-                        <span className="layout-topbar-item-text">Settings</span>
-                        <span className="layout-topbar-icon pi pi-cog"/>
-                    </button>
-                    <button className="p-link">
-                        <span className="layout-topbar-item-text">User</span>
-                        <span className="layout-topbar-icon pi pi-user"/>
-                    </button>
 					<button className="p-link layout-menu-button" onClick={this.props.onToggleMenu}>
                     <span className="pi pi-bars"/>
                     </button>
