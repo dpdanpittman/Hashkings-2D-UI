e3446817facafa9f0a82372da17c4f8126f0bb7f
Daniel Pittman <dpdanpittman@gmail.com>
2019-05-03 00:18:39 -0700

    removed calendar

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,7 +2,6 @@ import React, {Component} from "react";
 import {CarService} from "../service/CarService";
 import {Panel} from "primereact/panel";
 import {Chart} from "primereact/chart";
-import {FullCalendar} from "primereact/fullcalendar";
 import {HashkingsAPI, seedNames} from "../service/HashkingsAPI";
 import Cookie from "js-cookie";
 
@@ -131,83 +130,6 @@ export class Dashboard extends Component {
   }
 
   render() {
-    let fullcalendarOptions = {
-      defaultDate: "2017-02-01",
-      header: {
-        left: "prev,next today",
-        center: "title",
-        right: "month,agendaWeek,agendaDay"
-      },
-      editable: true
-    };
-
-    let events = [
-      {
-        id: 1,
-        title: "All Day Event",
-        start: "2017-02-01"
-      },
-      {
-        id: 2,
-        title: "Long Event",
-        start: "2017-02-07",
-        end: "2017-02-10"
-      },
-      {
-        id: 3,
-        title: "Repeating Event",
-        start: "2017-02-09T16:00:00"
-      },
-      {
-        id: 4,
-        title: "Repeating Event",
-        start: "2017-02-16T16:00:00"
-      },
-      {
-        id: 5,
-        title: "Conference",
-        start: "2017-02-11",
-        end: "2017-02-13"
-      },
-      {
-        id: 6,
-        title: "Meeting",
-        start: "2017-02-12T10:30:00",
-        end: "2017-02-12T12:30:00"
-      },
-      {
-        id: 7,
-        title: "Lunch",
-        start: "2017-02-12T12:00:00"
-      },
-      {
-        id: 8,
-        title: "Meeting",
-        start: "2017-02-12T14:30:00"
-      },
-      {
-        id: 9,
-        title: "Happy Hour",
-        start: "2017-02-12T17:30:00"
-      },
-      {
-        id: 10,
-        title: "Dinner",
-        start: "2017-02-12T20:00:00"
-      },
-      {
-        id: 11,
-        title: "Birthday Party",
-        start: "2017-02-13T07:00:00"
-      },
-      {
-        id: 12,
-        title: "Click for Google",
-        url: "http://google.com/",
-        start: "2017-02-28"
-      }
-    ];
-
     return (
       <div className="p-grid p-fluid dashboard">
         <div className="p-col-12 p-lg-4">
@@ -332,9 +254,6 @@ export class Dashboard extends Component {
           </Panel>
         </div>
         <div className="p-col-12 p-lg-8">
-          <Panel header="Gardening Schedule" style={{height: "100%"}}>
-            <FullCalendar events={events} options={fullcalendarOptions} />
-          </Panel>
         </div>
 
         <div className="p-col-12 p-lg-4">
