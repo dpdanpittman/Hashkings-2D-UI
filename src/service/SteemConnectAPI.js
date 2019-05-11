f2c4253e9a639e4e4ab075dad08832998d8ed15f
Jake Rawsthorne <jake@jakerawsthorne.co.uk>
2019-05-11 13:52:23 +0100

    SteemConnect callback environment variable

--- src/service/SteemConnectAPI.js
+++ src/service/SteemConnectAPI.js
@@ -3,9 +3,8 @@ import steemconnect from "steemconnect";
 const api = new steemconnect.Client({
   app: "review.app",
   baseURL: "https://steemconnect.com",
-  callbackURL: process.env.REACT_SC_CALLBACK
-    ? process.env.REACT_SC_CALLBACK
-    : "http://localhost:3000/callback",
+  callbackURL:
+    process.env.REACT_APP_SC_CALLBACK || "http://localhost:3000/callback",
   scope: ["custom_json", "login"]
 });
 
