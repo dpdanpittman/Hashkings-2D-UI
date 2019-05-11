a24afd2cc8fe0c4169627e24bc74a0a695db1590
Jake Rawsthorne <jake@jakerawsthorne.co.uk>
2019-05-11 13:32:21 +0100

    Maybe fix callback env

--- src/service/SteemConnectAPI.js
+++ src/service/SteemConnectAPI.js
@@ -3,8 +3,9 @@ import steemconnect from "steemconnect";
 const api = new steemconnect.Client({
   app: "review.app",
   baseURL: "https://steemconnect.com",
-  callbackURL:
-    process.env.REACT_SC_CALLBACK || "http://localhost:3000/callback",
+  callbackURL: process.env.REACT_SC_CALLBACK
+    ? process.env.REACT_SC_CALLBACK
+    : "http://localhost:3000/callback",
   scope: ["custom_json", "login"]
 });
 
