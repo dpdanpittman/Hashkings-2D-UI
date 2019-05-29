27b08e7db4569233e405aef7964c86754572a724
Daniel Pittman <dpdanpittman@gmail.com>
2019-05-29 00:18:34 -0700

    update color scheme

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -5,7 +5,7 @@ import _ from "lodash";
 export default function Inventory({user}) {
   return (
     <div className="p-grid">
-      <div className="card-blank-red p-col-4">
+      <div className="card-blank-red p-col-3">
         <h3>
           <u>Active Gardens</u>
           {_.uniqBy(user.activeGardens, garden => garden.id[0])
@@ -23,7 +23,8 @@ export default function Inventory({user}) {
             ))}
         </h3>
       </div>
-      <div className="card-blank-red p-col-4">
+	  <div className="p-col-1" />
+      <div className="card-blank-red p-col-3">
         <h3>
           <u>Available Gardens</u>
           {_.uniqBy(user.availableGardens, garden => garden[0])
@@ -41,7 +42,8 @@ export default function Inventory({user}) {
             ))}
         </h3>
       </div>
-      <div className="card-blank-red p-col-4">
+	  <div className="p-col-1" />
+      <div className="card-blank-red p-col-3">
         <h3>
           <u>Available Seeds</u>
           {_.uniqBy(user.availableSeeds, seed => seed.strain)
