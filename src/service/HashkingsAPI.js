cb0919433f84fbd93ce69eb6c4575b4e03a42152
Daniel Pittman <dpdanpittman@gmail.com>
2019-07-21 13:32:45 -0700

    244
    line 244 changed range

--- src/service/HashkingsAPI.js
+++ src/service/HashkingsAPI.js
@@ -241,7 +241,7 @@ export class HashkingsAPI {
         };
       })
       .sort((a, b) => b.xp - a.xp)
-      .slice(0, 20)
+      .slice(0, 82)
       .map((l, position) => ({...l, position: position + 1}));
 
     if (username) {
