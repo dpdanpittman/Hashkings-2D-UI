95b823f3e22ecbf253a9068c17c4439a1bf8573c
Daniel Pittman <dpdanpittman@gmail.com>
2019-08-28 01:10:36 -0700

    Update MarketPlots.js

--- src/components/MarketPlots.js
+++ src/components/MarketPlots.js
@@ -5,39 +5,11 @@ import {HashkingsAPI} from "../service/HashkingsAPI";
 import {StateContext} from "../App";
 import Delegate from "./Delegate";
 import BuyGarden from "./BuyGarden";
-import { makeStyles } from '@material-ui/core/styles';
-import Chip from '@material-ui/core/Chip';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import Divider from '@material-ui/core/Divider';
-import Typography from '@material-ui/core/Typography';
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    width: '100%',
-    maxWidth: 360,
-    backgroundColor: theme.palette.background.paper,
-  },
-  chip: {
-    marginRight: theme.spacing(1),
-  },
-  section1: {
-    margin: theme.spacing(3, 2),
-  },
-  section2: {
-    margin: theme.spacing(2),
-  },
-  section3: {
-    margin: theme.spacing(3, 1, 1),
-  },
-}));
 
 export const MarketPlots = () => {
   const {username} = useContext(StateContext);
   const [delegation, setDelegation] = useState({used: 0, available: 0});
   const [landSupply, setLandSupply] = useState();
-
-  const classes = useStyles();
 
   const hashkingsApi = new HashkingsAPI();
 
@@ -66,6 +38,20 @@ export const MarketPlots = () => {
     setDelegation(newDelegation);
   };
 
+  if (!username) {
+    return (
+      <div className="p-fluid">
+        <div className="p-grid">
+          <div className="p-col-12">
+            <div className="card-blank-green card-w-title">
+              <h1><a href="/login"><b><u>Please sign in to see your garden</u></b></a></h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   const images = [
     {
       original: "https://i.imgur.com/F3G7OJ1.png",
@@ -92,56 +78,7 @@ export const MarketPlots = () => {
       thumbnail: "https://i.imgur.com/IomdkMfl.png"
     }
   ];
-
-  if (!username) {
-    return (
-      <div className="p-fluid">
-        <div className="p-grid">
-          <div className="p-col-12">
-            <div className="card-blank-green card-w-title">
-              <h1><a href="/login"><b><u>Please sign in to see your garden</u></b></a></h1>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
   return (
-    /*material*/
-    <div className={classes.root}>
-      <div className={classes.section1}>
-        <Grid container alignItems="center">
-          <Grid item xs>
-            <Typography gutterBottom variant="h4">
-              Toothbrush
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Typography gutterBottom variant="h6">
-              $4.50
-            </Typography>
-          </Grid>
-        </Grid>
-        <Typography color="textSecondary" variant="body2">
-          Pinstriped cornflower blue cotton blouse takes you on a walk to the park or just down the
-          hall.
-        </Typography>
-      </div>
-      <Divider variant="middle" />
-      <div className={classes.section2}>
-        <Typography gutterBottom variant="body1">
-          Select type
-        </Typography>
-        <div>
-          <Chip className={classes.chip} label="Extra Soft" />
-          <Chip className={classes.chip} color="primary" label="Soft" />
-          <Chip className={classes.chip} label="Medium" />
-          <Chip className={classes.chip} label="Hard" />
-        </div>
-      </div>
-      <div className={classes.section3}>
-        <Button color="primary">Add to cart</Button>
-      </div>
     <div className="p-fluid">
       <div className="p-grid">
         <div className="p-col-12">
@@ -194,7 +131,6 @@ export const MarketPlots = () => {
           </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
