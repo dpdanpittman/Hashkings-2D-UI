940370cc370da45d9de817d16ce3d384d80d7c5c
Daniel Pittman <dpdanpittman@gmail.com>
2019-11-08 16:07:19 -0800

    validation

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -10,7 +10,6 @@ import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import { HashkingsAPI } from "../service/HashkingsAPI";
@@ -90,7 +89,7 @@ export const LoginPage = ({history}) => {
 
   useEffect(() => {
     hashkingsApi.steemUserExists(username).then(username => {
-      if (username && username === username) {
+      if (username) {
         setValidatedTo(username);
       } else {
         setValidatedTo();
