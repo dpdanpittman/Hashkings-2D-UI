433f1208ba81e1765afad5eb322739a23b6f218b
Daniel Pittman <dpdanpittman@gmail.com>
2019-12-27 02:43:12 -0800

    Update WelcomeCard.js

--- src/components/WelcomeCard.js
+++ src/components/WelcomeCard.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
 import { ParallaxBanner } from 'react-scroll-parallax';
 
 const useStyles = makeStyles({
@@ -18,20 +16,9 @@ export default function WelcomeCard() {
 
   return (
     <div>
-    <ParallaxBanner
-    className={classes.media}
-    layers={[
-        {
-            image: 'https://i.imgur.com/lLmUeqa.png',
-            amount: -.1,
-            expanded: true,
-        },
-    ]}
-    style={{
-        height: '100px',
-    }}
->
-</ParallaxBanner>
+      <center>
+    <img src="https://i.imgur.com/cepuuEG.png"></img>
+    </center>
     </div>
   );
 }
