ce8c51cfb49a84e9ad53e73ef37a1959b33132e5
Daniel Pittman <dpdanpittman@gmail.com>
2020-03-29 22:38:31 -0700

    update state

--- src/service/SteemConnectAPI.js
+++ src/service/SteemConnectAPI.js
@@ -1,8 +1,8 @@
 import steemconnect from "steemconnect";
 
 const api = new steemconnect.Client({
-  app: "loginking",
-  baseURL: "https://steemconnect.com",
+  app: "hive-loginking",
+  baseURL: "https://hivesigner.com/",
   callbackURL:
     process.env.REACT_APP_SC_CALLBACK || "http://localhost:3000/callback",
   scope: ["custom_json", "login"]
